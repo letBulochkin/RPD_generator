d7898b41e5406d096e652ef86a2083c0b10f78e2
Letchik Bulochkin <fir-anton@yandex.ru>
2018-11-22 12:37:46 +0300

    interface update

--- main.py
+++ main.py
@@ -14,12 +14,12 @@ class RPD_Window(QMainWindow):
 
 		self.PLAN_FNAME = None
 		self.SAMPLE_FNAME = None
-		self.RPD = None
+		self.RPD = None  # может не надо?
 
 		self.BOXES = [[self.ui.uploadBox, self.ui.uploadBoxLabel, True], 
 			[self.ui.disciplineBox, self.ui.disciplineBoxLabel, False]]
 
-		self.ui.backButton.clicked.connect(self.browse_box)
+		self.ui.backButton.clicked.connect(self.browse_box)  # можно перенести в on_createButton_clicked
 		self.ui.forwButton.clicked.connect(self.browse_box)
 		self.ui.study_planUploadButton.clicked.connect(self.on_planButton_clicked)
 		self.ui.rpdUploadButton.clicked.connect(self.on_rpdButton_clicked)
@@ -57,8 +57,32 @@ class RPD_Window(QMainWindow):
 		if self.PLAN_FNAME != None and self.SAMPLE_FNAME != None:
 			self.RPD = commutator.rpd(self.PLAN_FNAME, self.SAMPLE_FNAME)
 			self.ui.uploadStatusLabel.setText('Успешно!')
+			self.ui.dispComboBox.addItems(
+				[i[0] + ' ' + i[1] for i in self.RPD.STUDY_PLAN.list_avail_disciplines()])
+			self.ui.dispComboBox.activated.connect(self.on_dispComboBox_activated)
 		else:
 			self.ui.uploadStatusLabel.setText('Не выбраны файлы!')
+
+	@QtCore.pyqtSlot(int)
+	def on_dispComboBox_activated(self, index):
+		self.RPD.create_discipline(
+			self.sender().itemText(self.sender().currentIndex()).rsplit(' ')[0])
+		self.ui.dispNameEdit.setText(self.RPD.DISCIPLINE.NAME)
+		self.ui.dispFieldEdit.setText(self.RPD.STUDY_PLAN.FIELD_OF_KNOW)
+		self.ui.dispProfileEdit.setText(self.RPD.STUDY_PLAN.PROFILE)
+		self.ui.dispInstituteEdit.setText(self.RPD.STUDY_PLAN.INSTITUTE)
+		self.ui.dispFormEdit.setText(self.RPD.STUDY_PLAN.EDU_FORMAT)
+		self.ui.dispProgEdit.setText(self.RPD.STUDY_PLAN.EDU_PROG)
+		self.ui.dispCathEdit.setText(self.RPD.STUDY_PLAN.CATHEDRA)
+		if self.RPD.DISCIPLINE.PART == True:
+			self.ui.dispPartBaseButton.setChecked(True)
+		else:
+			self.ui.dispPartVarButton.setChecked(True)
+		if self.RPD.DISCIPLINE.OBLIGATION == True:
+			self.ui.dispObligTrueButton.setChecked(True)
+		else:
+			self.ui.dispObligFalseButton.setChecked(True)
+		#print(self.sender().itemText(self.sender().currentIndex()).rsplit(' ')[0])
 
 if __name__ == "__main__":
 	app = QApplication(sys.argv)
